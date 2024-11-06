823532939f87f775810156a6c2beda3abe1bcbba
zengyh1900 <zengyh1900@gmail.com>
2024-11-06 22:04:02 +0800

    update: 2024

--- .dev_scripts/main.py
+++ .dev_scripts/main.py
@@ -1,9 +1,9 @@
-import re 
-import os 
-import csv 
-from tqdm import tqdm 
+import re
+import os
+import csv
+from tqdm import tqdm
 import argparse
-import functools 
+import functools
 
 parser = argparse.ArgumentParser(description = 'What the program does')
 parser.add_argument('-f', choices=['csv', 'md'], default='md')
@@ -18,93 +18,96 @@ HEAD = f"""# Awesome-Inpainting-Tech
 
 A curated list of inpainting papers and resources, inspired by [awesome-computer-vision](https://github.com/jbhuang0604/awesome-computer-vision).
 
-This `README.md` is automatically generated from [`.dev_scripts/collection.csv`](.dev_scripts/collection.csv). 
+This `README.md` is automatically generated from [`.dev_scripts/collection.csv`](.dev_scripts/collection.csv).
 
-We provide [scripts](.dev_scripts/main.py) to automatically generate `README.md` from CSV file or vice versa. 
+We provide [scripts](.dev_scripts/main.py) to automatically generate `README.md` from CSV file or vice versa.
 
 Welcome to pull request to update or correct this collection. 🥰
 """
 
-def readme_to_csv(): 
-    # save all data to csv file 
+def readme_to_csv():
+    # save all data to csv file
     csvfile = open(COLLECTION_CSV, 'w')
     csv_writer = csv.writer(csvfile, delimiter=',')
     csv_writer.writerow(['Year', 'Conf', 'Type', 'Ttitle', 'URL', 'Code', 'Project'])
 
-    # parse data from readme 
+    # parse data from readme
     with open(MD_FILE, 'r', encoding='utf-8') as md:
         lines = md.readlines()
 
-    type = None 
+    type = None
     papers = []
-    i = 0 
-    while i < len(lines): 
-        if '## Year' not in lines[i]: 
-            i += 1 
+    i = 0
+    while i < len(lines):
+        if '## Year' not in lines[i]:
+            i += 1
         else:
             year = int(re.match(r'## Year (.*)', lines[i]).groups()[0])
-            i += 1 
+            i += 1
             while i < len(lines) and (r'## Year' not in lines[i]):
                 line = lines[i]
                 conf, type, title, url = re.match(
                     r'- \*\*(.*)\*\* \((.*)\) \[(.*)\]\((.*)\)\..*', line).groups()
-                
-                project, code = None, None 
+
+                project, code = None, None
                 if '[code]' in line:
                     code = line.split('[[code]](')[-1].split(')')[0]
                 if '[project]' in line:
                     project = line.split('[[project]](')[-1].split(')')[0]
-                
+
                 papers.append(dict(title=title, url=url, conf=conf, year=year, project=project, code=code, type=type))
-                i += 1 
+                i += 1
 
     papers = sorted(papers, key=lambda x: (x['year'], x['conf'], x['type']), reverse=True)
-    for p in papers: 
+    for p in papers:
         csv_writer.writerow([p['year'], p['conf'], p['type'], p['title'], p['url'], p['code'], p['project']])
     csvfile.close()
 
 
 def csv_to_readme():
-    # save all data to csv file 
+    # save all data to csv file
     csvfile = open(COLLECTION_CSV)
     csv_reader = csv.reader(csvfile, delimiter=',')
-    
+
     papers = {}
-    # parse data from csv file 
+    # parse data from csv file
     for idx, row in enumerate(csv_reader):
         if idx == 0:
-            continue 
-        year, conf, type, title, url, code, project = row 
+            continue
+        try:
+            year, conf, type, title, url, code, project = row
+        except:
+            print(row)
         p = dict(title=title, url=url, conf=conf, year=year, project=project, code=code, type=type)
-        if str(year) not in papers: 
+        if str(year) not in papers:
             papers[str(year)] = [p]
         else:
             papers[str(year)].append(p)
-    
-    for k, v in papers.items(): 
+
+    for k, v in papers.items():
         papers[k].sort(key=lambda x: (x['year'], x['conf'], x['type']), reverse=True)
-    
+
     message = {}
     # generate msg from parsed dict data
     years = sorted([y for y in list(papers.keys())], reverse=True)
     for k,v in papers.items():
         msg = f"## Year {k}\n"
-        for p in v: 
+        for p in v:
             msg += f"- **{p['conf']}** ({p['type']}) [{p['title']}]({p['url']})."
             if p['code']:
                 msg += f" [[code]]({p['code']}) "
             if p['project']:
                 msg += f" [[project]]({p['project']}) "
             msg += "\n"
-        message[k] = msg 
-    
-    # write to readme 
+        message[k] = msg
+
+    # write to readme
     readme_content = HEAD
-    for y in years: 
+    for y in years:
         readme_content += message[y]
     with open(MD_FILE, 'w') as f:
         f.write(readme_content)
- 
+
 
 
 
